7dc4143f308a36d34933c6ecfa93487cf34b7de9
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-04-16 15:36:03 -0300

    Configuração do módulo de home

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:omdb_flutter/app/modules/home/home_module.dart';
 
-class AppModule extends Module {}
+class AppModule extends Module {
+  @override
+  List<Bind<Object>> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ModuleRoute('/', module: HomeModule()),
+      ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:omdb_flutter/app/app_module.dart';
+import 'package:omdb_flutter/app/app_widget.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: const MyApp()));
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Omdb Flutter App',
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
-    );
-  }
+void main() {
+  return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
