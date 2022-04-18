62db7e94235b05ec83634ee087f4a8583ebcd98b
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-04-18 16:53:15 -0300

    Export dos execptions no core

--- infrastructure/core/lib/core.dart
+++ infrastructure/core/lib/core.dart
@@ -2,3 +2,5 @@ library core;
 
 export 'styles/colors.dart';
 export 'styles/text_styles.dart';
+export 'exceptions/exceptions.dart';
+export 'failures/failures.dart';
--- infrastructure/core/test/core_test.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:core/core.dart';
-
-void main() {}
