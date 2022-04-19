c803320a81707b402aa19a39d6c91c2c8729ca77
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-04-18 22:42:32 -0300

    Criado bloc e testes unitários

--- features/search/lib/src/presentation/bloc/search_bloc.dart
+++ features/search/lib/src/presentation/bloc/search_bloc.dart
@@ -1,7 +1,40 @@
 import 'package:bloc/bloc.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:search/src/domain/usecase/search_movies.dart';
 import 'package:search/src/presentation/bloc/search_event.dart';
 import 'package:search/src/presentation/bloc/search_state.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
-  SearchBloc() : super(SearchEmptyState());
+  final SearchMoviesUseCaseContract _searchMoviesUseCase;
+
+  SearchBloc({
+    required SearchMoviesUseCaseContract searchMoviesUseCase,
+  })  : _searchMoviesUseCase = searchMoviesUseCase,
+        super(SearchEmptyState()) {
+    on<OnQueryChanged>((event, emit) => _searchMovies(event, emit),
+        transformer: debounce(const Duration(milliseconds: 700)));
+  }
+
+  Future<void> _searchMovies(
+    OnQueryChanged event,
+    Emitter<SearchState> emit,
+  ) async {
+    emit(SearchLoadingState());
+    final query = event.query;
+    final result = await _searchMoviesUseCase.execute(query: query);
+
+    result.fold((failure) {
+      emit(SearchErrorState(message: failure.message));
+    }, (data) {
+      if (data.search.isEmpty) {
+        emit(SearchEmptyState());
+      } else {
+        emit(SearchSuccessState(result: data));
+      }
+    });
+  }
+}
+
+EventTransformer<SearchEvent> debounce<SearchEvent>(Duration duration) {
+  return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
 }
