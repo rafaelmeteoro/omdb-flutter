f9cee167131bd582bf238a3a2ffc58c8b47ba32c
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-04-18 23:44:43 -0300

    Deletado arquivos não usados'

--- lib/app/modules/home/domain/repositories/home_repository_interface.dart
+++
@@ -1,7 +0,0 @@
-import 'package:omdb_flutter/app/modules/home/domain/entities/home_entity.dart';
-
-abstract class IHomeMovieRepository {
-  Future<ResultSearch> searchMovies({
-    required String title,
-  });
-}
