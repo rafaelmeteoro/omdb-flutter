309c7854bc1bdbc7717dd7f1a68f7a0536b739c8
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-07-06 17:05:03 -0300

    Criado modulo app

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:omdb_flutter/app/app_module.dart';
-import 'package:omdb_flutter/app/app_widget.dart';
-
-void main() {
-  return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
-}
