30adb872045e733ce0712a3170f91f03e97dbdb6
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-07-08 19:53:50 -0300

    unit test failure

---
+++ infrastructure/app_core/test/src/core/errors/failure_test.dart
@@ -0,0 +1,28 @@
+import 'package:app_core/failures.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class CustomFailure extends Failure {
+  CustomFailure(String? message) : super(message);
+}
+
+void main() {
+  group('failure', () {
+    test('custom failure', () {
+      // Arrange
+      final exception = CustomFailure('custom failure');
+
+      // Assert
+      expect(exception, isA<CustomFailure>());
+      expect(exception, isA<Failure>());
+      expect(exception.message, equals('custom failure'));
+    });
+
+    test('has failures equal', () {
+      // Arrange
+      final custom = CustomFailure('failure message');
+
+      // Assert
+      expect(custom == CustomFailure('failure message'), true);
+    });
+  });
+}
