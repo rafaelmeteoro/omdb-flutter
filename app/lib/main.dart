304f35939ea032f0f779c0f1dfcdff8d3bebf87c
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-07-09 23:17:52 -0300

    Ajuste imports

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:omdb_flutter/app/app_module.dart';
-import 'package:omdb_flutter/app/app_widget.dart';
+
+import 'app/app_module.dart';
+import 'app/app_widget.dart';
 
 void main() {
   return runApp(
