95c85345fa5584738d4d5f05d1e5fbd3cc04a65d
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-07-11 15:31:32 -0300

    Removido arquivos não usados

--- features/movie/test/full_coverage_test.dart
+++ features/movie/test/full_coverage_test.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: unused_import
 import 'package:movie/movie.dart';
-import 'package:movie/src/infra/datasource/movie_detail_remote_datasource_interface.dart';
-import 'package:movie/src/infra/repositories/movie_detail_repository.dart';
 import 'package:movie/src/core/typedef/signatures.dart';
 import 'package:movie/src/core/errors/failure.dart';
 import 'package:movie/src/features/movie/data/dto/movie_detail_dto.dart';
@@ -23,16 +21,5 @@ import 'package:movie/src/features/movie/presentation/widgets/movie_detail_info_
 import 'package:movie/src/features/movie/presentation/widgets/movie_detail_content.dart';
 import 'package:movie/src/features/movie/presentation/widgets/movie_detail_wishlist_button.dart';
 import 'package:movie/src/movie_module.dart';
-import 'package:movie/src/external/datasource/movie_detail_remote_datasource.dart';
-import 'package:movie/src/external/adapter/rating_adapter.dart';
-import 'package:movie/src/external/adapter/movie_detail_adapter.dart';
-import 'package:movie/src/domain/repositories/movie_detail_repository_interface.dart';
-import 'package:movie/src/domain/errors/failures.dart';
-import 'package:movie/src/domain/usecase/get_movie_detail.dart';
-import 'package:movie/src/domain/entities/movie_detail.dart';
-import 'package:movie/src/domain/entities/rating.dart';
-import 'package:movie/src/presentation/bloc/movie_bloc.dart';
-import 'package:movie/src/presentation/bloc/movie_state.dart';
-import 'package:movie/src/presentation/bloc/movie_event.dart';
 
 void main() {}
