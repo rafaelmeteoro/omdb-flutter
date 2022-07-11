a55a6c42aded367de9da420f32a56b1b175282bc
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-07-11 15:51:44 -0300

    Removendo custom exception

--- infrastructure/core/test/full_coverage_test.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: unused_import
-import 'package:core/presentation.dart';
-import 'package:core/exceptions/exceptions.dart';
-import 'package:core/styles/colors.dart';
-import 'package:core/styles/text_styles.dart';
-import 'package:core/domain.dart';
-
-void main() {}
