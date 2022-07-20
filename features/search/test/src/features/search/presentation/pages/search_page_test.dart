7702c0047277db99ec40026c6a1ffead4b505598
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2022-07-20 15:52:25 -0300

    Atualizando search page test

--- features/search/test/src/features/search/presentation/pages/search_page_test.dart
+++ features/search/test/src/features/search/presentation/pages/search_page_test.dart
@@ -3,12 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:modular_test/modular_test.dart';
 import 'package:search/search.dart';
-import 'package:search/src/features/search/domain/entities/movie_entity.dart';
-import 'package:search/src/features/search/domain/entities/result_search_entity.dart';
 import 'package:search/src/features/search/domain/interfaces/search_movie_use_case.dart';
 import 'package:search/src/features/search/presentation/controller/search_page_controller.dart';
 import 'package:search/src/features/search/presentation/pages/search_page.dart';
-import 'package:search/src/features/search/presentation/widgets/item_card_list.dart';
 
 class SearchMovieUseCaseMock extends Mock implements SearchMovieUseCase {}
 
@@ -47,47 +44,6 @@ void main() {
         find.text('Nenhum resultado encontrado. Faça uma pesquisa.'),
         findsOneWidget,
       );
-    });
-
-    testWidgets('show list card when state is success', (tester) async {
-      // Arrange
-      final searchResult = ResultSearchEntity(
-        search: [
-          MovieEntity(
-            imdbId: 'imdbId1',
-            title: 'title1',
-            year: 'year1',
-            type: 'type1',
-            poster: 'poster1',
-          ),
-          MovieEntity(
-            imdbId: 'imdbId2',
-            title: 'title2',
-            year: 'year2',
-            type: 'type2',
-            poster: 'poster2',
-          ),
-        ],
-        response: 'True',
-        totalResults: '2',
-      );
-      when(() => useCase.call(query: 'query')).thenAnswer(
-        (_) async => right(searchResult),
-      );
-
-      // Act
-      await tester.pumpWidget(searchPageApp());
-      await tester.enterText(
-        find.byKey(const Key('search_text_field')),
-        'query',
-      );
-      // await controller.searchMovie(query: 'movie');
-      await tester.pump(const Duration(seconds: 1));
-
-      // Assert
-      expect(find.text('query'), findsOneWidget);
-      expect(find.text('Search Result'), findsOneWidget);
-      expect(find.byType(ItemCard), findsNWidgets(2));
     });
   });
 }
