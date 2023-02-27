7316f6f1d465c0f33c34ff787e670116809eede5
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-02-27 19:07:43 -0300

    Ajuste

--- infrastructure/dev_core/lib/dev_core.dart
+++ infrastructure/dev_core/lib/dev_core.dart
@@ -1,5 +1,5 @@
 library dev_core;
 
+export 'package:flutter_test/flutter_test.dart';
 export 'package:fpdart/fpdart.dart';
 export 'package:mocktail/mocktail.dart';
-export 'package:flutter_test/flutter_test.dart';
