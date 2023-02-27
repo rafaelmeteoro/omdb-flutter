0cf1c186c177f0e06e214cb8f1be4aabc9a9a588
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-02-26 21:04:17 -0300

    Add teste search page delegate

---
+++ features/search/test/src/features/search/presentation/pages/search_page_delegate_test.dart
@@ -0,0 +1,32 @@
+import 'package:core/presentation.dart';
+import 'package:dev_core/dev_core.dart';
+import 'package:search/src/features/search/presentation/pages/search_page_delegate.dart';
+
+class ModularNavigateMock extends Mock implements IModularNavigator {}
+
+void main() {
+  late IModularNavigator navigate;
+  late SearchPageDelegate delegate;
+
+  setUp(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    navigate = ModularNavigateMock();
+    Modular.navigatorDelegate = navigate;
+    delegate = SearchPageFlow();
+  });
+
+  group(SearchPageFlow, () {
+    test('call movie page when onItemSelected is called', () async {
+      // Arrange
+      when(
+        () => navigate.pushNamed(any(), arguments: any(named: 'arguments')),
+      ).thenAnswer((_) async => Future.value());
+
+      // Act
+      await delegate.onItemSearchSelected(movieId: 'movieId');
+
+      // Assert
+      verify(() => navigate.pushNamed('/movie', arguments: 'movieId'));
+    });
+  });
+}
