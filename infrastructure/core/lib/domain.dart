96fa45952139bb72c46eb12055a7d0c6eb239943
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-02-28 21:26:08 -0300

    Adicionado lint dart style

--- infrastructure/core/lib/domain.dart
+++ infrastructure/core/lib/domain.dart
@@ -1,3 +1,3 @@
-export 'package:fpdart/fpdart.dart';
-export 'package:equatable/equatable.dart';
 export 'package:dio/dio.dart';
+export 'package:equatable/equatable.dart';
+export 'package:fpdart/fpdart.dart';
--- infrastructure/core/lib/presentation.dart
+++ infrastructure/core/lib/presentation.dart
@@ -1,8 +1,9 @@
 library core;
 
-export 'styles/colors.dart';
-export 'styles/text_styles.dart';
+export 'package:animate_do/animate_do.dart';
+export 'package:cached_network_image/cached_network_image.dart';
 export 'package:flutter_modular/flutter_modular.dart';
 export 'package:rxdart/rxdart.dart';
-export 'package:cached_network_image/cached_network_image.dart';
-export 'package:animate_do/animate_do.dart';
+
+export 'styles/colors.dart';
+export 'styles/text_styles.dart';
