961109796c0ad1e59a75e61cac5a77d839cce5a9
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-03-01 23:41:51 -0300

    Melhorando cobertura (#67)

--- features/movie/test/src/features/movie/presentation/widgets/movie_detail_info_list_test.dart
+++ features/movie/test/src/features/movie/presentation/widgets/movie_detail_info_list_test.dart
@@ -4,22 +4,29 @@ import 'package:movie/src/features/movie/presentation/widgets/movie_detail_info_
 
 void main() {
   group(MovieDetailInfoList, () {
-    Widget detailInfoApp() {
+    Widget detailInfoApp(List<String> values) {
       return MaterialApp(
         home: Material(
           child: MovieDetailInfoList(
             title: 'title',
-            values: ['value1', 'value2'],
+            values: values,
           ),
         ),
       );
     }
 
-    testWidgets('show infor in detail info list', (tester) async {
-      await tester.pumpWidget(detailInfoApp());
+    testWidgets('show info in detail info list', (tester) async {
+      await tester.pumpWidget(detailInfoApp(['value1', 'value2']));
 
       expect(find.byType(MovieDetailInfoList), findsOneWidget);
       expect(find.text('title: value1, value2'), findsOneWidget);
+    });
+
+    testWidgets('show info in detail with info empty', (tester) async {
+      await tester.pumpWidget(detailInfoApp([]));
+
+      expect(find.byType(MovieDetailInfoList), findsOneWidget);
+      expect(find.text('title: '), findsOneWidget);
     });
   });
 }
