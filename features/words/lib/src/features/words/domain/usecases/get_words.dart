80e521dc3896835bcb7267529c7991594e63cdfb
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-03-04 17:01:59 -0300

    Usecase get lista de palavras

---
+++ features/words/lib/src/features/words/domain/usecases/get_words.dart
@@ -0,0 +1,16 @@
+import '../../../../core/typedef/signatures.dart';
+import '../interfaces/get_words_storage_use_case.dart';
+import '../interfaces/words_storage_repository.dart';
+
+class GetWords implements GetWordsStorageUseCase {
+  GetWords({
+    required WordsStorageRepository repository,
+  }) : _repository = repository;
+
+  final WordsStorageRepository _repository;
+
+  @override
+  Future<ResultGetWordsStorage> call() {
+    return _repository.getWords();
+  }
+}
