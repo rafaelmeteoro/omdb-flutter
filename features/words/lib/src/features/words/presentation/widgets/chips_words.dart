84cd5c4dd80b1a61ac54356ea5c81243e8f36f4d
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-03-06 15:37:39 -0300

    Adicionado função para deletar chip

--- features/words/lib/src/features/words/presentation/widgets/chips_words.dart
+++ features/words/lib/src/features/words/presentation/widgets/chips_words.dart
@@ -3,17 +3,20 @@ import 'package:flutter/material.dart';
 class ChipsWords extends StatelessWidget {
   const ChipsWords({
     super.key,
-    required this.words,
-  });
+    required List<String> words,
+    required Function(String word) onDeletePressed,
+  })  : _words = words,
+        _onDeletePressed = onDeletePressed;
 
-  final List<String> words;
+  final List<String> _words;
+  final Function(String word) _onDeletePressed;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Wrap(
-        children: words
+        children: _words
             .map(
               (word) => Padding(
                 padding: const EdgeInsets.all(4.0),
@@ -26,6 +29,9 @@ class ChipsWords extends StatelessWidget {
                     child: Text(word[0].toUpperCase()),
                   ),
                   label: Text(word),
+                  onDeleted: () {
+                    _onDeletePressed(word);
+                  },
                 ),
               ),
             )
