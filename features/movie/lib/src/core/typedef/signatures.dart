bcd2a8ccbc630f817b7f6d5d1976edd2301f7633
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-03-09 12:23:52 -0300

    Movie storage repository contains

--- features/movie/lib/src/core/typedef/signatures.dart
+++ features/movie/lib/src/core/typedef/signatures.dart
@@ -3,3 +3,4 @@ import 'package:app_core/app_core.dart';
 import '../../features/movie/domain/entities/movie_detail_entity.dart';
 
 typedef MovieDetail = Result<MovieDetailEntity>;
+typedef ResultContainsMovieStorage = Result<bool>;
