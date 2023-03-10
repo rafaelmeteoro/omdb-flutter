3304b6fb37312b221d09da1b96707211c0c7e640
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-03-09 23:23:22 -0300

    Delete add usecase e remove usecase

--- features/movie/lib/src/features/movie/domain/interfaces/add_movie_storage_use_case.dart
+++
@@ -1,8 +0,0 @@
-import '../../../../core/typedef/signatures.dart';
-import '../entities/movie_detail_entity.dart';
-
-abstract class AddMovieStorageUseCase {
-  Future<ResultAddRemoveMovieStorage> call({
-    required MovieDetailEntity movie,
-  });
-}
--- features/movie/lib/src/features/movie/domain/interfaces/remove_movie_storage_use_case.dart
+++
@@ -1,8 +0,0 @@
-import '../../../../core/typedef/signatures.dart';
-import '../entities/movie_detail_entity.dart';
-
-abstract class RemoveMovieStorageUseCase {
-  Future<ResultAddRemoveMovieStorage> call({
-    required MovieDetailEntity movie,
-  });
-}
