b6267255d8babeb85935678e49f5c514e8288287
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-06-08 21:29:14 -0300

    Removido widgets module

--- features/words/lib/src/words_module.dart
+++ features/words/lib/src/words_module.dart
@@ -1,5 +1,4 @@
 import 'package:core/presentation.dart';
-import 'package:flutter/widgets.dart';
 import 'package:words_storage_manager/words_storage_manager.dart';
 
 import 'features/words/data/local_words_storage_repository.dart';
@@ -49,41 +48,4 @@ class WordsModule extends Module {
           child: (context, args) => const WordsPage(),
         )
       ];
-}
-
-class WordsWidgetModule extends WidgetModule {
-  WordsWidgetModule({super.key});
-
-  @override
-  List<Bind<Object>> get binds => [
-        // Repository
-        Bind.lazySingleton<WordsStorageRepository>(
-          (i) => LocalWordsStorageRepository(
-            storage: i.get<WordsStorage>(),
-          ),
-        ),
-
-        // UseCase
-        Bind.lazySingleton<GetWordsStorageUseCase>(
-          (i) => GetWords(
-            repository: i.get<WordsStorageRepository>(),
-          ),
-        ),
-        Bind.lazySingleton<DeleteWordsStorageUseCase>(
-          (i) => DeleteWords(
-            repository: i.get<WordsStorageRepository>(),
-          ),
-        ),
-
-        // Controller
-        Bind.lazySingleton<WordsPageController>(
-          (i) => WordsPageController(
-            getWordsStorageUseCase: i.get<GetWordsStorageUseCase>(),
-            deleteWordsStorageUseCase: i.get<DeleteWordsStorageUseCase>(),
-          ),
-        ),
-      ];
-
-  @override
-  Widget get view => const WordsPage();
 }
