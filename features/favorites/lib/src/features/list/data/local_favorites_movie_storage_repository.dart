ef3f9025944d29c09bc780babed2573eff706db4
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2023-06-09 17:56:01 -0300

    Adicionado repository favorites

---
+++ features/favorites/lib/src/features/list/data/local_favorites_movie_storage_repository.dart
@@ -0,0 +1,26 @@
+import 'package:app_core/app_core.dart';
+import 'package:movie_storage_manager/movie_storage_manager.dart';
+
+import '../../../core/errors/failure.dart';
+import '../../../core/typedef/signatures.dart';
+import '../domain/interfaces/favorite_movie_storage_repository.dart';
+import 'dto/favorite_movie_dto.dart';
+
+class LocalFavoritesMovieStorageRepository implements FavoriteMovieStorageRepository {
+  LocalFavoritesMovieStorageRepository({
+    required MovieStorage storage,
+  }) : _storage = storage;
+
+  final MovieStorage _storage;
+
+  @override
+  Future<ResultFavorites> getFavorites() async {
+    try {
+      final result = await _storage.readAll();
+      final favorites = result.map((e) => FavoriteMovieDto.fromJson(e as JsonFormat)).toList();
+      return ResultFavorites.right(favorites);
+    } catch (e) {
+      return ResultFavorites.left(FavoritesStorageFailure(message: e.toString()));
+    }
+  }
+}
